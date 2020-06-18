7db2091b3780d63d7645d78736c14c4ee5996e1d
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-18 17:16:50 +0300

    [Add] view current timer left

--- app/src/main/java/com/example/udemylearntimer/MainActivity.java
+++ app/src/main/java/com/example/udemylearntimer/MainActivity.java
@@ -10,17 +10,28 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static final int DEFAULT_START = 30;
-    SeekBar timerLeft;
+
+    SeekBar timerLeftBar;
     Button button;
-    TextView timerValue;
+    TextView timerValueView;
+    Integer timerVal = this.DEFAULT_START;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timerLeft = findViewById(R.id.leftTimerBar);
+        timerLeftBar = findViewById(R.id.leftTimerBar);
         button = findViewById(R.id.startBtn);
-        timerValue = findViewById(R.id.timerValue);
+        timerValueView = findViewById(R.id.timerValue);
+        viewTimerVal();
+    }
+
+    private void viewTimerVal() {
+        Integer minutes = Math.round(timerVal / 60);
+        Integer seconds = timerVal - minutes * 60;
+        String minView = String.format("%1$2s", minutes).replace(' ', '0');
+        String secView = String.format("%1$2s", seconds).replace(' ', '0');
+        timerValueView.setText(minView + ":" + secView);
     }
 }
