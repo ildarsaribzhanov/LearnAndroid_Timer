de7ea5ee2529c2aea34a0e8c0f7a97b6aae9272f
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-18 17:08:32 +0300

    [Add] bind elements

--- app/src/main/java/com/example/udemylearntimer/MainActivity.java
+++ app/src/main/java/com/example/udemylearntimer/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.udemylearntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int DEFAULT_START = 30;
+    SeekBar timerLeft;
+    Button button;
+    TextView timerValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerLeft = findViewById(R.id.leftTimerBar);
+        button = findViewById(R.id.startBtn);
+        timerValue = findViewById(R.id.timerValue);
     }
 }
