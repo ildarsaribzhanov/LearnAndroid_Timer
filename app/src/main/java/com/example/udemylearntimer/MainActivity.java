beb04d05d8f3ba453e809a0a0d8d52c3d4ac59a3
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-21 16:37:31 +0300

    [Add] timer processing

--- app/src/main/java/com/example/udemylearntimer/MainActivity.java
+++ app/src/main/java/com/example/udemylearntimer/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.udemylearntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -15,6 +18,34 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView timerValueView;
     Integer timerVal = 30;
+    CountDownTimer timer;
+
+    public class MyCountDownTimer extends CountDownTimer {
+        /**
+         * @param millisInFuture    The number of millis in the future from the call
+         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+         *                          is called.
+         * @param countDownInterval The interval along the way to receive
+         *                          {@link #onTick(long)} callbacks.
+         */
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            Log.d("MyTimer", String.valueOf(millisUntilFinished / 1000));
+            timerVal = Math.round(millisUntilFinished / 1000);
+            timerLeftBar.setProgress(timerVal);
+            viewTimerVal(timerVal);
+        }
+
+        @Override
+        public void onFinish() {
+            Log.d("MyTimer", "Finished");
+            stopTimer();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.startBtn);
         timerValueView = findViewById(R.id.timerValue);
-        viewTimerVal();
+        viewTimerVal(timerVal);
 
         timerLeftBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -35,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 if (fromUser) {
                     timerVal = progress;
                     timerLeftBar.setProgress(timerVal);
-                    viewTimerVal();
+                    viewTimerVal(timerVal);
                 }
             }
 
@@ -49,13 +80,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
-    private void viewTimerVal() {
-        Integer minutes = Math.round(timerVal / 60);
-        Integer seconds = timerVal - minutes * 60;
+    private void viewTimerVal(Integer val) {
+        Integer minutes = Math.round(val / 60);
+        Integer seconds = val - minutes * 60;
         String minView = String.format("%1$2s", minutes).replace(' ', '0');
         String secView = String.format("%1$2s", seconds).replace(' ', '0');
         timerValueView.setText(minView + ":" + secView);
+    }
+
+    public void toggleTimer(View view) {
+        if (timer == null) {
+            timer = new MyCountDownTimer(timerVal * 1000, 1000);
+        }
+
+        if (button.getText() == "Stop") {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    public void startTimer() {
+        timer.start();
+        button.setText("Stop");
+        timerLeftBar.setEnabled(false);
+    }
+
+    public void stopTimer() {
+        timer.cancel();
+        button.setText("Start");
+        timerLeftBar.setEnabled(true);
+        timer = null;
     }
 }
